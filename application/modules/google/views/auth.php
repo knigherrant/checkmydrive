ddb5b66e337906348a04fc7714b28d7609d6354a
Hoàng Long <longcuxit@gmail.com>
2017-08-12 13:58:08 +0700

    update author css

--- application/modules/google/views/auth.php
+++ application/modules/google/views/auth.php
@@ -11,15 +11,24 @@ $user = Checkmydrive::getUser();
 $config  = Checkmydrive::getConfigs();
 ?>
 
+<style>
 
+</style>
 <div id="jk-contents" class="ct_ctn">
     <div class="contents row-fluid">
         <div id="sidebar-left"><?php echo CheckmydriveHelper::buildSidebar();?></div>
-        <div id="content-wrapper">
-            <div class="row-fluid ct_subcont">
-                <a href="<?php
-                echo $this->client->createAuthUrl()
-                        ?>">Access Google</a>
+        <div id="content-wrapper" style="position: relative;">
+            <div class="row-fluid ct_subcont box-balance">
+                <h2>Authorization required</h2>
+                <p>Authorize this app in Google Drive</p>
+                <a 
+                    class ="btn btn-primary btn-google-auth"
+                    href="<?php
+                        echo $this->client->createAuthUrl()
+                        ?>"> Authorize</a>
+                <p>
+                    <label><input type="checkbox" name="remember"/><span>Remember me</span></label>
+                </p>
             </div> 
         </div>
     </div>
